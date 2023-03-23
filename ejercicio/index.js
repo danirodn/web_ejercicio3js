1747ce9710ece01684eb2e3192a1a851abd8734b
danirodn <107278808+danirodn@users.noreply.github.com>
2023-03-23 10:00:44 -0400

    Update index.js

--- ejercicio/index.js
+++ ejercicio/index.js
@@ -1,35 +1,45 @@
+// Pedimos los datos del empleado
+const cedula = parseInt(prompt("Ingrese la cedula: "));
+let nombre = prompt("Ingrese nombre: ");
+let apellido = prompt("Ingrese apellido: ");
+const horasDiurnas = parseInt(prompt("Ingrese la cantidad de horas diurnas trabajadas: "));
 
-function calcularSueldoQuincenal() {
-  var nombre = document.getElementById("nombre").value;
-  var apellido = document.getElementById("apellido").value;
-  var cedula = document.getElementById("cedula").value;
-  var salario = parseFloat(document.getElementById("salario").value);
-  // Calcular el sueldo quincenal sin deducciones
-  var sueldoQuincenal = salario / 2;
+const horasVespertinas = parseInt(prompt("Ingrese la cantidad de horas vespertinas trabajadas: "));
+const horasNocturnas = parseInt(prompt("Ingrese la cantidad de horas nocturnas trabajadas: "));
 
-  // Calcular los descuentos correspondientes
-  var ahorroHabitacional = 0;
-  var seguroSocial = 0;
-  if (salario < 85000) {
-      ahorroHabitacional = salario * 0.001;
-      seguroSocial = salario * 0.0015;
-  } else if (salario < 150000) {
-      ahorroHabitacional = salario * 0.0015;
-      seguroSocial = salario * 0.002;
-  } else {
-      ahorroHabitacional = salario * 0.003;
-      seguroSocial = salario * 0.0025;
-  }
+// Definimos el costo por hora para cada turno
+const costoHoraDiurna = 675;
+const costoHoraVespertina = 700;
+const costoHoraNocturna = 956.23;
 
-  // Calcular el sueldo quincenal con deducciones
-  var sueldoQuincenalConDeducciones = sueldoQuincenal - ahorroHabitacional - seguroSocial;
+// Calculamos el salario bruto
+const salarioBruto = (horasDiurnas * costoHoraDiurna) + (horasVespertinas * costoHoraVespertina) + (horasNocturnas * costoHoraNocturna);
 
+// Calculamos los descuentos
+let descuentoAhorroHabitacional = 0;
+let descuentoSeguroSocial = 0;
 
-  // Hacer referencia a los elementos HTML y mostrar los resultados
-  document.getElementById("nombreCompleto").innerHTML = nombre + " " + apellido;
-  document.getElementById("cedulaMostrada").innerHTML = cedula;
-  document.getElementById("sueldoQuincenalCalculado").innerHTML = sueldoQuincenal.toFixed(2);
-  document.getElementById("ahorroHabitacionalCalculado").innerHTML = ahorroHabitacional.toFixed(2);
-  document.getElementById("seguroSocialCalculado").innerHTML = seguroSocial.toFixed(2);
-  document.getElementById("sueldoQuincenalConDeduccionesCalculado").innerHTML = sueldoQuincenalConDeducciones.toFixed(2);
-  }
+if (salarioBruto < 85000) {
+  descuentoAhorroHabitacional = salarioBruto * 0.0015;
+  descuentoSeguroSocial = salarioBruto * 0.002;
+} else if (salarioBruto > 150000) {
+  descuentoAhorroHabitacional = salarioBruto * 0.003;
+  descuentoSeguroSocial = salarioBruto * 0.0025;
+}
+
+// Calculamos el salario neto y quincenal
+const salarioNeto = salarioBruto - descuentoAhorroHabitacional - descuentoSeguroSocial;
+const salarioQuincenal = salarioNeto / 2;
+
+// Imprimimos los resultados
+document.write("Calculadora de sueldo quincenal<br>")
+document.write("<br> Cedula: " ,cedula , "<br>")
+document.write("Nombre: ", nombre , "<br>")
+document.write("Apellido: ", apellido , "<br>")
+
+document.write(`El salario bruto es: $${salarioBruto.toFixed(2)}`);
+document.write(`<br>El descuento de ahorro habitacional es: $${descuentoAhorroHabitacional.toFixed(2)}`);
+document.write(`<br>El descuento de seguro social es: $${descuentoSeguroSocial.toFixed(2)}`);
+document.write(`<br>El salario neto es: $${salarioNeto.toFixed(2)}`);
+document.write(`<br>El salario quincenal es: $${salarioQuincenal.toFixed(2)}`);
+
